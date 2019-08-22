4508125d51558405ae5db8903b1d2a144d606def
Alisa Soroka <alissoroka@gmail.com>
2019-08-22 20:10:55 +0300

    Add simple encryption

--- UIScriptable/Assets/Saving/GenericClass/SaverSO.cs
+++ UIScriptable/Assets/Saving/GenericClass/SaverSO.cs
@@ -10,27 +10,37 @@ namespace SavingSystem
     public class SaverSO<T> : ScriptableObject where T : class
     {
         public string filename = "PersistentData";
+        //Can be null
         public T persistentData;
+        [HideInInspector]//not sure about it
         public bool isFirstApplicationLauch = true;
-        public bool enabled = true;
+        public bool autoLoad = true;
+        public bool encrypt = true;
 
-        private string jsonString;
+        private string key = "123451234512345123451234512345DF";
 
         void OnEnable()
         {
-            if (enabled)
+            if (autoLoad)
             {
                 Debug.Log(this.name + " Enabled");
 
                 LoadProgres();
-                persistentData = GetData();
                 Application.quitting += SaveProgres;
+
+                RijndaelEncryption crypto = new RijndaelEncryption();
+                byte[] soup = crypto.Encrypt("Some text to encrypt", "123451234512345123451234512345DF");
+                string res = crypto.Decrypt(soup, "123451234512345123451234512345DF");
+                //FileStream file = File.Create(Application.persistentDataPath + filename);
+                //File.WriteAllBytes(file, soup);
+                //byte[] soup = File.ReadAllBytes(Application.persistentDataPath + filename);
+                Debug.Log(res);
             }
         }
 
         void OnDestroy()
         {
-            if (enabled)
+            if (autoLoad)
             {
                 Debug.Log(this.name + " Destroyed");
 
@@ -38,7 +48,7 @@ namespace SavingSystem
             }
         }
 
-        private T GetData()
+        private T GetDataFromJson(string jsonString)
         {
             //TODO : try - catch
             T res;
@@ -54,10 +64,24 @@ namespace SavingSystem
             Debug.Log("Saving progress");
             try
             {
-                BinaryFormatter bf = new BinaryFormatter();
-                FileStream file = File.Create(Application.persistentDataPath + filename);
-                bf.Serialize(file, jsonString);
-                file.Close();
+                string jsonString = JsonUtility.ToJson(persistentData);
+                string path = Application.persistentDataPath + filename;
+                if (encrypt)
+                {
+                    RijndaelEncryption crypto = new RijndaelEncryption();
+                    byte[] soup = crypto.Encrypt(jsonString, key);
+
+                    File.WriteAllBytes(path, soup);
+                }
+                else
+                {
+                    BinaryFormatter bf = new BinaryFormatter();
+                    FileStream file = File.Create(path);
+
+                    bf.Serialize(file, jsonString);
+                    file.Close();
+                }
+                
                 Debug.Log("Progress saved");
             }
             catch (Exception e)
@@ -72,11 +96,26 @@ namespace SavingSystem
             {
                 try
                 {
-                    BinaryFormatter bf = new BinaryFormatter();
-                    FileStream file = File.Open(Application.persistentDataPath + filename, FileMode.Open);
-                    jsonString = (string)bf.Deserialize(file);
-                    file.Close();
+                    string path = Application.persistentDataPath + filename;
+                    string jsonString;
+
+                    if (encrypt)
+                    {
+                        RijndaelEncryption crypto = new RijndaelEncryption();
+                        
+                        byte[] soup = File.ReadAllBytes(Application.persistentDataPath + filename);
+                        jsonString = crypto.Decrypt(soup, key);
+                    }
+                    else
+                    {
+                        BinaryFormatter bf = new BinaryFormatter();
+                        FileStream file = File.Open(Application.persistentDataPath + filename, FileMode.Open);
+                        jsonString = (string)bf.Deserialize(file);
+                        file.Close();
+                    }
+                    
                     isFirstApplicationLauch = false;
+                    persistentData = GetDataFromJson(jsonString);
                     Debug.Log("Found progress file");
                 }
                 catch (Exception e)
@@ -86,7 +125,7 @@ namespace SavingSystem
             }
             else
             {
-                jsonString = "";
+                persistentData = Activator.CreateInstance<T>();
             }
         }// LoadProgres
 
@@ -97,7 +136,7 @@ namespace SavingSystem
                 try
                 {
                     File.Delete(Application.persistentDataPath + filename);
-                   // persistentData = new T();
+                    persistentData = Activator.CreateInstance<T>();
                 }
                 catch (Exception e)
                 {
---
+++ UIScriptable/Assets/Saving/RijndaelEncryption.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class RijndaelEncryption
+{
+
+    public string Decrypt(byte[] soup, string key)
+    {
+        try
+        {
+            byte[] iv = Encoding.ASCII.GetBytes("0123456789012345"); //must be 16 chars
+            byte[] keyBytes = Encoding.ASCII.GetBytes(key);
+            // Create a new instance of the Rijndael
+            // class.  This generates a new key and initialization 
+            // vector (IV).
+            using (Rijndael myRijndael = Rijndael.Create())
+            {
+                myRijndael.Key = keyBytes;
+                myRijndael.IV = iv;
+
+                // Encrypt the string to an array of bytes.
+                //byte[] encrypted = EncryptStringToBytes(original, myRijndael.Key, myRijndael.IV);
+                //return encrypted;
+                // Decrypt the bytes to a string.
+                string roundtrip = DecryptStringFromBytes(soup, myRijndael.Key, myRijndael.IV);
+                return roundtrip;
+                ////Display the original data and the decrypted data.
+                //Debug.Log("Original:   {0} " + original);
+                //Debug.Log("Round Trip: {0} " + roundtrip);
+
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: {0}", e.Message);
+            return null;
+        }
+    }
+
+    public byte[] Encrypt(string original, string key)//must be 32 chars
+    {
+        try
+        {
+            byte[] iv = Encoding.ASCII.GetBytes("0123456789012345"); //must be 16 chars
+            byte[] keyBytes = Encoding.ASCII.GetBytes(key);
+            // Create a new instance of the Rijndael
+            // class.  This generates a new key and initialization 
+            // vector (IV).
+            using (Rijndael myRijndael = Rijndael.Create())
+            {
+                myRijndael.Key = keyBytes;
+                myRijndael.IV = iv;
+
+                // Encrypt the string to an array of bytes.
+                byte[] encrypted = EncryptStringToBytes(original, myRijndael.Key, myRijndael.IV);
+                return encrypted;
+                // Decrypt the bytes to a string.
+                //string roundtrip = DecryptStringFromBytes(encrypted, myRijndael.Key, myRijndael.IV);
+
+                ////Display the original data and the decrypted data.
+                //Debug.Log("Original:   {0} " + original);
+                //Debug.Log("Round Trip: {0} " + roundtrip);
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: {0}", e.Message);
+            return null;
+        }
+    }
+
+    static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+    {
+        // Check arguments.
+        if (plainText == null || plainText.Length <= 0)
+            throw new ArgumentNullException("plainText");
+        if (Key == null || Key.Length <= 0)
+            throw new ArgumentNullException("Key");
+        if (IV == null || IV.Length <= 0)
+            throw new ArgumentNullException("IV");
+        byte[] encrypted;
+        // Create an Rijndael object
+        // with the specified key and IV.
+        using (Rijndael rijAlg = Rijndael.Create())
+        {
+            rijAlg.Key = Key;
+            rijAlg.IV = IV;
+
+            // Create an encryptor to perform the stream transform.
+            ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+            // Create the streams used for encryption.
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+
+                        //Write all data to the stream.
+                        swEncrypt.Write(plainText);
+                    }
+                    encrypted = msEncrypt.ToArray();
+                }
+            }
+        }
+
+
+        // Return the encrypted bytes from the memory stream.
+        return encrypted;
+
+    }
+
+    static string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV)
+    {
+        // Check arguments.
+        if (cipherText == null || cipherText.Length <= 0)
+            throw new ArgumentNullException("cipherText");
+        if (Key == null || Key.Length <= 0)
+            throw new ArgumentNullException("Key");
+        if (IV == null || IV.Length <= 0)
+            throw new ArgumentNullException("IV");
+
+        // Declare the string used to hold
+        // the decrypted text.
+        string plaintext = null;
+
+        // Create an Rijndael object
+        // with the specified key and IV.
+        using (Rijndael rijAlg = Rijndael.Create())
+        {
+            rijAlg.Key = Key;
+            rijAlg.IV = IV;
+
+            // Create a decryptor to perform the stream transform.
+            ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+            // Create the streams used for decryption.
+            using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+
+                        // Read the decrypted bytes from the decrypting stream
+                        // and place them in a string.
+                        plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+
+        }
+
+        return plaintext;
+
+    }
+
+}
